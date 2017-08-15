33aba17b352900a161f7979a4e5e1bff44c0de65
Don Bedwell <timetourst@gmail.com>
2017-08-14 22:32:15 -0400

    10:30PM commit

--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,53 @@
 // import ORM/create functions to interact with db
-var orm = require("../config/orm.js");
+//var orm = require("../config/orm.js");
 //object with methods to pass off info to premade queries
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+// var burger = {
+//   all: function(cb) {
+//     orm.all("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
+//   // The variables cols and vals are arrays
+//   create: function(cols, vals, cb) {
+//     orm.create("burgers", cols, vals, function(res) {
+//       cb(res);
+//     });
+//   },
+//   update: function(objColVals, condition, cb) {
+//     orm.update("burgers", objColVals, condition, function(res) {
+//       cb(res);
+//     });
+//   },
+//   delete: function(condition, cb) {
+//     orm.delete("burgers", condition, function(res) {
+//       cb(res);
+//     });
+//   }
+// };
+// // Export db functions for the controller (burger_controller.js).
+// module.exports = burger;
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    text: {
+      burger_name: DataTypes.STRING,
+      // AllowNull is a flag that restricts a todo from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our todo is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      // defaultValue is a flag that defaults a new todos complete value to false if
+      // it isn't supplied one
+      defaultValue: false
+    },
+    date:{
+      type: DataTypes.NOW
+    }
+  });
+  return Burger;
 };
-// Export db functions for the controller (burger_controller.js).
-module.exports = burger;
--- server.js
+++ server.js
@@ -7,13 +7,21 @@ var port = process.env.PORT || 3000;
 // create instance of express
 var app = express();
 
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 // use html interpreter from body-parser npm
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 // override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+//app.use(methodOverride("_method"));
 
 // Set Handlebars instance
 var exphbs = require("express-handlebars");
@@ -22,8 +30,20 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // import routes
-var routes = require("./controllers/burger_controller.js");
+//var routes = require("./controllers/burger_controller.js");
 // give server access to routes
-app.use("/", routes);
+//app.use("/", routes);
 // port listener
-app.listen(port);
+//app.listen(port);
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
