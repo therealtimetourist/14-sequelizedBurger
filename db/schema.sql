feccd8699f127904e5203ac20add7ae2d3c23218
Don Bedwell <timetourst@gmail.com>
2017-08-14 21:59:06 -0400

    jaws db created

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
+DROP DATABASE IF EXISTS sequelizedBurger_db;
+CREATE DATABASE sequelizedBurger_db;
+USE sequelizedBurger_db;
 
-CREATE USER 'burgers_db_user'@'localhost' IDENTIFIED BY 'toorregrub';
-GRANT ALL ON burgers_db.* TO 'burgers_db_user'@'localhost';
+CREATE USER 'sequelizedBurger_db_user'@'localhost' IDENTIFIED BY 'toorregrub';
+GRANT ALL ON sequelizedBurger_db.* TO 'burgers_db_user'@'localhost';
 FLUSH PRIVILEGES;
 
 CREATE TABLE burgers (
